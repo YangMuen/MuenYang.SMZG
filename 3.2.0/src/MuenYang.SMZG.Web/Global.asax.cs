d6f16c9e230f9452be86ba3551b16b0f0e143e2d
YangMuen <450659781@qq.com>
2018-04-17 23:20:04 +0800

    ABP可以部署，屏蔽英文配置

--- 3.2.0/src/MuenYang.SMZG.Web/Global.asax.cs
+++ 3.2.0/src/MuenYang.SMZG.Web/Global.asax.cs
@@ -4,6 +4,13 @@ using Abp.Web;
 using Castle.Facilities.Logging;
 using Abp.WebApi.Validation;
 using System.Threading;
+using Abp.Configuration;
+using Abp.Localization;
+using System.Globalization;
+using System.Web;
+using Abp.Extensions;
+using Abp.Logging;
+using Abp.Timing;
 
 namespace MuenYang.SMZG.Web
 {
@@ -26,6 +33,34 @@ namespace MuenYang.SMZG.Web
             //{
             //    Console.WriteLine(exception);
             //    throw;
+            //}
+        }
+
+        protected override void Session_Start(object sender, EventArgs e)
+        {
+            RestoreUserLanguage();
+            base.Session_Start(sender, e);
+        }
+
+        private void RestoreUserLanguage()
+        {
+            Response.Cookies.Add(new HttpCookie("Abp.Localization.CultureName", "zh-CN") { Expires = Clock.Now.AddYears(2) });
+            //var settingManager = AbpBootstrapper.IocManager.Resolve<ISettingManager>();
+            //var defaultLanguage = settingManager.GetSettingValue(LocalizationSettingNames.DefaultLanguage);
+
+            //if (defaultLanguage.IsNullOrEmpty())
+            //{
+            //    return;
+            //}
+
+            //try
+            //{
+            //    CultureInfo.GetCultureInfo(defaultLanguage);
+            //    Response.Cookies.Add(new HttpCookie("Abp.Localization.CultureName", defaultLanguage) { Expires = Clock.Now.AddYears(2) });
+            //}
+            //catch (CultureNotFoundException exception)
+            //{
+            //    LogHelper.Logger.Warn(exception.Message, exception);
             //}
         }
     }
